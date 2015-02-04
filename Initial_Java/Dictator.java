b2df37953dde9092ca510828053450364db2ff14
Sky Johnson <jsky.johnson@gmail.com>
2015-02-03 23:35:14 -0700

    stuff today

---
+++ Initial_Java/Dictator.java
@@ -0,0 +1,51 @@
+import java.util.*;
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.JFrame;
+
+
+/**
+ * Dictator of all game aspects, and manager of inputs and the what not.
+ * 
+ * @author Sky Johnson
+ * 
+ */
+public class Dictator extends JFrame
+{
+    // Threads
+    Thread initThread;
+    Thread loopThread;
+    
+    //Game Constants
+    
+    /**
+     * Constructor for objects of class Dictator
+     */
+    public Dictator()
+    {
+        // initialise instance variables
+       
+    }
+    
+    /**
+     * 
+     */
+    public void start()
+    {
+        if (loopThread == null){
+            
+        }
+    }
+    
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return 4;
+    }
+}
